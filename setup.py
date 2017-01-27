541ed00d7304f3b6e5360ecd06eed5a16979c954
bingtel <bingtel@qq.com>
2017-01-27 10:04:28 +0800

    添加setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='hare',
+      version='0.1',
+      description='A python ORM based on pymysql with ActiveRecord',
+      long_description=long_description,
+      keywords='python ORM orm ActiveRecord pymysql raw sql',
+      url='https://github.com/bingtel/hare',
+      author='bingtel Wang',
+      author_email='bingtelwang@163.com',
+      license='MIT',
+      packages=['hare'],
+      install_requires=[
+          'PyMySQL',
+      ],
+      zip_safe=True)
